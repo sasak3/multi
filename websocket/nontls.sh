c584c950ea6e07cfcf633ea245db3111981000ba
sasak3 <152268056+sasak3@users.noreply.github.com>
2023-12-07 10:15:40 +0800

    Update nontls.sh

--- websocket/nontls.sh
+++ websocket/nontls.sh
@@ -21,7 +21,7 @@ echo "Progress..." | lolcat
 sleep 3
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/SETANTAZVPN/multi-ws/ipuk/websocket/ws-nontls.py
+wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/sasak3/multi-ws/ipuk/websocket/ws-nontls.py
 chmod +x /usr/local/bin/ws-nontls
 
 # Installing Service
